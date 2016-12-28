1a389f546bba410c4b885509a835a7b9058f96a2
Arun Saini <Arun.Saini@akqa.com>
2016-12-28 12:20:15 +0530

    Linting

--- apps/game1/src/script/playerUi.js
+++ apps/game1/src/script/playerUi.js
@@ -7,30 +7,30 @@ let player = {};
 const renderPlayerUi = function(player, playerUi) {
     playerUi.style.left = player.positionX + 'px';
     playerUi.style.top = player.positionY + 'px';
-}
+};
 
 const movePlayer = function(player, direction = 'ArrowDown') {
-    if (direction === "ArrowDown") {
+    if (direction === 'ArrowDown') {
         playerModule.rules.moveDown.call(player);
-    } else if  (direction === "ArrowUp") {
+    } else if  (direction === 'ArrowUp') {
         playerModule.rules.moveUp.call(player);
-    } else if  (direction === "ArrowLeft") {
+    } else if  (direction === 'ArrowLeft') {
         playerModule.rules.moveLeft.call(player);
-    } else if  (direction === "ArrowRight") {
+    } else if  (direction === 'ArrowRight') {
         playerModule.rules.moveRight.call(player);
     } 
-}
+};
 
 const uiInteraction = function(event) {
-    if (event.type === "keydown") {
+    if (event.type === 'keydown') {
         movePlayer(player, event.key);
-        playerUiModule.renderPlayerUi(player, playerUi);
+        renderPlayerUi(player, playerUi);
     }
-}
+};
 
 const init = function() {
-    let playerUi = document.querySelector('.player');
-    let player = playerModule.playerFactory('John');
-}
+    playerUi = document.querySelector('.player');
+    player = playerModule.playerFactory('John');
+};
 
 module.exports = {init, renderPlayerUi, movePlayer, uiInteraction};
--- apps/game1/src/script/worldUi.js
+++ apps/game1/src/script/worldUi.js
@@ -1,21 +1,21 @@
 'use strict';
 const playerUiModule = require('./playerUi.js');
 
-let worldUi = {};
+//let worldUi = {};
 
 
 const uiInteraction = function(event) {
     playerUiModule.uiInteraction(event);
-}
+};
 
 const bindEvents = function() {
     window.addEventListeners('keydown', uiInteraction);
-}
+};
 
 const init = function() {
-    worldUi = document.querySelector('.world');
+    //worldUi = document.querySelector('.world');
     playerUiModule.init();
     bindEvents();
-}
+};
 
 module.exports = {init};
