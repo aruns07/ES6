a3c2b8aa465497f16d35e0b6a204dea950662a03
Arun Saini <Arun.Saini@akqa.com>
2016-12-29 20:07:25 +0530

    explore Web assembly

---
+++ wasm/add.asm.js
@@ -0,0 +1,11 @@
+function Module() {
+    "use asm";
+    function add(x, y) {
+        x = x | 0;
+        y = y | 0;
+        return (x + y | 0);
+    }
+    return {add: add};
+}
+
+module.exports = Module;
