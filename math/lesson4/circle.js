c15a37307a265d5b29a53f337a6e35db65333666
Arun Saini <Arun.Saini@akqa.com>
2017-01-08 19:10:27 +0530

    lesson 4 circle path

--- math/lesson4/circle.js
+++ math/lesson4/circle.js
@@ -7,16 +7,30 @@
 
     let centerX = width / 2,
         centerY = height / 2,
-        radius = 100,
-        angle = 0.1;
+        radius = 200,
+        angle = 0,
+        numberOfSteps = 30,
+        sliceSize = (Math.PI * 2) / numberOfSteps,
+        x, y;
 
     const SPEED = 0.05;
     
     function render() {
-        let x = centerX + radius * Math.cos(angle);
-        let y = centerY + radius * Math.sin(angle);
+
 
         context.clearRect(0, 0, width, height);
+
+        for (var slice = 0 ; slice < (Math.PI * 2); slice += sliceSize) {
+            x = centerX + radius * Math.cos(slice);
+            y = centerY + radius * Math.sin(slice);
+            context.beginPath();
+            context.arc(x, y, 10, 0, Math.PI * 2, false);
+            context.fill();
+        }
+
+
+        x = centerX + radius * Math.cos(angle);
+        y = centerY + radius * Math.sin(angle);
         context.beginPath();
         context.arc(centerX, centerY, 2, 0, Math.PI * 2, false);
         context.arc(x, y, 10, 0, Math.PI * 2, false);
