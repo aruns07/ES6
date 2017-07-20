cf88808a3171b4a5dbfa26b9afe852a6c4e43169
Arun Saini <arun.saini07@gmail.com>
2017-07-20 10:14:39 +0530

    Math: vector

---
+++ math/lesson7/vector.js
@@ -0,0 +1,76 @@
+class Vector {
+    constructor(x, y) {
+        this._x = x;
+        this._y = y;
+    }
+
+    set x(value) {
+        this._x = value;
+    }
+
+    get x() {
+        return this._x;
+    }
+
+    set y(value) {
+        this._y = value;
+    }
+
+    get y() {
+        return this._y;
+    }
+
+    set angle(value) {
+        this._x = Math.cos(value) * this.length;
+        this._y = Math.sin(value) * this.length;
+    }
+
+    get angle() {
+        return Math.atan2(this._y, this._x);
+    }
+
+    set length(value) {
+        this._x = Math.cos(this.angle) * value;
+        this._y = Math.sin(this.angle) * value;
+    }
+
+    get length() {
+        return Math.sqrt(this._x * this._x + this._y * this._y);
+    }
+
+    add(v2) {
+        return new Vector(this._x + v2.x, this._y + v2.y);
+    }
+
+    subtract(v2) {
+        return new Vector(this._x - v2.x, this._y - v2.y);
+    }
+
+    multiply(value) {
+        return new Vector(this._x * value, this._y * value);
+    }
+
+    divide(value) {
+        return new Vector(this._x / value, this._y / value);
+    }
+
+    addTo(value) {
+        this._x = this._x + value;
+        this._y = this._y + value;
+    }
+
+    subtractFrom(value) {
+        this._x = this._x - value;
+        this._y = this._y - value;
+    }
+
+    multiplyBy(value) {
+        this._x = this._x * value;
+        this._y = this._y * value;
+    }
+
+    divideBy(value) {
+        this._x = this._x / value;
+        this._y = this._y / value;
+    }
+}
