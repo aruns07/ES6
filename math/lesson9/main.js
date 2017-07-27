7972c2d27b8ff65512d285827ae16cff32fedc46
Arun Saini <arun.saini07@gmail.com>
2017-07-27 09:42:02 +0530

    Math: Gravity

--- math/lesson9/main.js
+++ math/lesson9/main.js
@@ -2,15 +2,22 @@ import {Vector} from '../lesson7/vector.js';
 import {Particle} from '../lesson8/particle.js';
 import {canvas, context, width, height} from '../canvas.js';
 
-const particle = new Particle(100, height, 10, -Math.PI / 3, 0.1);
+const particles = [];
+const particleCount = 400;
+
+for (let index = 0; index< particleCount; index++) {
+    particles.push(new Particle(width / 2, height/ 2, Math.random() * 6 + 1, Math.random() * Math.PI * 2, 0.05));
+}
 
 const update = () => {
     context.clearRect(0, 0, width, height);
 
-    particle.update();
-    context.beginPath();
-    context.arc(particle.position.x, particle.position.y, 10, 0, Math.PI * 2, false);
-    context.fill();
+    particles.forEach(particle => {
+        particle.update();
+        context.beginPath();
+        context.arc(particle.position.x, particle.position.y, 5, 0, Math.PI * 2, false);
+        context.fill();
+    });
 
     requestAnimationFrame(update);
 
