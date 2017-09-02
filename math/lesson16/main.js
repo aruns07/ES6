aca961587a7bed64f11c4d0941a0e035be0b2a32
Arun Saini <arun.saini07@gmail.com>
2017-09-02 10:24:11 +0530

    Math: lesson16 spring multiple weights

--- math/lesson16/main.js
+++ math/lesson16/main.js
@@ -2,12 +2,23 @@ import {Vector} from '../lesson7/vector.js';
 import {Particle} from '../lesson13/particle.js';
 import {canvas, context, width, height} from '../canvas.js';
 
-const weight = new Particle(width * Math.random(), height * Math.random());
-weight.friction = 0.9;
-const point = new Vector(width/2, height/2);
+const weight1 = new Particle(width * Math.random(), height * Math.random());
+weight1.friction = 0.85;
+
+const weight2 = new Particle(width * Math.random(), height * Math.random());
+weight2.friction = 0.85;
+
+const weight3 = new Particle(width * Math.random(), height * Math.random());
+weight3.friction = 0.85;
+
+const weight4 = new Particle(width * Math.random(), height * Math.random());
+weight4.friction = 0.85;
+
+const weight5 = new Particle(width * Math.random(), height * Math.random());
+weight5.friction = 0.85;
+
 const separation = 100;
-const springConstant = 0.05;
-let forceVector;
+const springConstant = 0.07;
 
 /*
 window.addEventListener('mousemove', (event) => {
@@ -15,26 +26,65 @@ window.addEventListener('mousemove', (event) => {
     point.y = event.clientY;
 });
 */
+
+const applySpringForce = (weight1, weight2, separation) => {
+    let forceVector = weight1.position.subtract(weight2.position);
+    forceVector.length = forceVector.length - separation;
+    forceVector.multiplyBy(springConstant);
+    weight2.accelerate = forceVector;
+    weight2.update();
+
+    forceVector.multiplyBy(-1);
+    weight1.accelerate = forceVector;
+    weight1.update();
+};
+
 const update = () => {
     context.clearRect(0, 0, width, height);
 
-    forceVector = point.subtract(weight.position);
-    forceVector.length = forceVector.length - separation;
-    forceVector.multiplyBy(springConstant);
-    weight.accelerate = forceVector;
-    weight.update();
+    applySpringForce(weight1, weight2, separation);
+    applySpringForce(weight1, weight3, separation);
+    applySpringForce(weight1, weight4, separation);
+    applySpringForce(weight1, weight5, separation);
 
     context.beginPath();
-    context.arc(weight.position.x, weight.position.y, 20, 0, Math.PI * 2, false);
+    context.arc(weight1.position.x, weight1.position.y, 20, 0, Math.PI * 2, false);
     context.fill();
 
     context.beginPath();
-    context.arc(point.x, point.y, 2, 0, Math.PI * 2, false);
+    context.arc(weight2.position.x, weight2.position.y, 20, 0, Math.PI * 2, false);
     context.fill();
 
     context.beginPath();
-    context.moveTo(point.x, point.y);
-    context.lineTo(weight.position.x, weight.position.y);
+    context.arc(weight3.position.x, weight3.position.y, 20, 0, Math.PI * 2, false);
+    context.fill();
+
+    context.beginPath();
+    context.arc(weight4.position.x, weight4.position.y, 20, 0, Math.PI * 2, false);
+    context.fill();
+
+    context.beginPath();
+    context.arc(weight5.position.x, weight5.position.y, 20, 0, Math.PI * 2, false);
+    context.fill();
+
+    context.beginPath();
+    context.moveTo(weight1.position.x, weight1.position.y);
+    context.lineTo(weight2.position.x, weight2.position.y);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(weight1.position.x, weight1.position.y);
+    context.lineTo(weight3.position.x, weight3.position.y);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(weight1.position.x, weight1.position.y);
+    context.lineTo(weight4.position.x, weight4.position.y);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(weight1.position.x, weight1.position.y);
+    context.lineTo(weight5.position.x, weight5.position.y);
     context.stroke();
 
     requestAnimationFrame(update);
