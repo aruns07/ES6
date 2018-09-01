0e27378bf70976785515020e72641c162099a8a3
Arun Saini <arun.saini07@gmail.com>
2018-09-01 18:20:38 +0530

    ES: test class scope

---
+++ ES/class-scope/first.js
@@ -0,0 +1,12 @@
+class SomeClass {
+    constructor() {
+        this.name = 'Some Class';
+    }
+    sayName() {
+        console.log(this.name);
+    }
+}
+
+let letInFirst = 10;
+var varInFirst = 20;
+const constInFirst = 30;
---
+++ ES/class-scope/main.js
@@ -0,0 +1,6 @@
+let someObject = new SomeClass();
+someObject.sayName();
+
+console.log(letInFirst);
+console.log(varInFirst);
+console.log(constInFirst);
